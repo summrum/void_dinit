dbce4421a33317a40149d04625235595befa82af
summrum <summrum@email.com>
2022-01-18 14:26:14 +0000

    master

--- etc/dinit.d/late-filesystems.sh
+++ etc/dinit.d/late-filesystems.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 . /etc/dinit.d/config/late-filesystems.conf
-if [ ! -z "$LATE_FILESYSTEMS" ]; then
-    mount -a -t "$LATE_FILESYSTEMS"
+if [ "$1" != "stop" ]; then
+    if [ ! -z "$LATE_FILESYSTEMS" ]; then
+        mount -at "$LATE_FILESYSTEMS"
+    fi
+else
+    if [ ! -z "$LATE_FILESYSTEMS" ]; then
+        umount -aft "$LATE_FILESYSTEMS"
+        ret=$?
+        [ $ret -ne 0 ] && umount -aflt "$LATE_FILESYSTEMS"
+        umount -aflt "$LATE_FILESYSTEMS"
+    fi
 fi
