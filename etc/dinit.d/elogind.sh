0892ebc4aea05b3ced5e76e9f16159950220b9aa
Laurence <pczlab@gmail.com>
2022-02-08 08:06:32 +0000

    update elogind service

---
+++ etc/dinit.d/elogind.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# adapted from Void Linux elogind.wrapper script
+cgroup=/sys/fs/cgroup/elogind
+
+mkdir -p "$cgroup"
+if ! mountpoint "$cgroup" > /dev/null; then
+	mount -t cgroup -o none,name=elogind cgroup $cgroup || exit 1
+fi
+
+[ ! -d /run/systemd ] && install -m755 -g 0 -o 0 -d /run/systemd
+[ ! -d /run/user ] && install -m755 -g 0 -o 0 -d /run/user
+for tmpfs in /run/systemd /run/user; do
+	mountpoint "$tmpfs" > /dev/null && continue
+	mkdir -p "$tmpfs"
+	mount -t tmpfs -o nosuid,nodev,noexec,mode=0755 none "$tmpfs" || exit 1
+done
+
+exec /usr/libexec/elogind/elogind
