54c120facf58d22de649f5343aa779ac3d733bd2
Laurence <pczlab@gmail.com>
2023-03-16 08:53:54 +0000

    add seedrng and update scripts

--- etc/dinit.d/scripts/apparmor.sh
+++ etc/dinit.d/scripts/apparmor.sh
@@ -10,14 +10,14 @@
 echo "Loading AppArmor profiles..."
 
 if [ -n "$APPARMOR" ]; then
-	if [ "$APPARMOR" != "complain" -a "$APPARMOR" != "enforce" ]; then
+	if [ "$APPARMOR" != "complain" ] && [ "$APPARMOR" != "enforce" ]; then
 		printf '! AppArmor set to %s - ignoring profiles\n' "$APPARMOR"
 		return
 	fi
 
 	[ "$APPARMOR" = "complain" ] && AACOMPLAIN="-C"
 
-	if [ -d /etc/apparmor.d -a -x /usr/bin/apparmor_parser ]; then
+	if [ -d /etc/apparmor.d ] && [ -x /usr/bin/apparmor_parser ]; then
 		apparmor_parser -a $AACOMPLAIN $(find /etc/apparmor.d -maxdepth 1 -type f ! -name '*.new-*_*')
 	else
 		printf '! AppArmor installation problem - ensure you have installed apparmor package\n'
--- etc/dinit.d/scripts/late-filesystems.sh
+++ etc/dinit.d/scripts/late-filesystems.sh
@@ -3,17 +3,22 @@
 
 case "$1" in
     start)
-		if [ ! -z "$LATE_FILESYSTEMS" ]; then
+		if [ -n "$LATE_FILESYSTEMS" ]; then
 		    mount -a -t "$LATE_FILESYSTEMS"
 		fi
 		;;
 	stop)
-		if [ ! -z "$LATE_FILESYSTEMS" ]; then
+		if [ -n "$LATE_FILESYSTEMS" ]; then
 		    umount -a -f -t "$LATE_FILESYSTEMS"
-		    ret=$?
-		    [ $ret -ne 0 ] && umount -a -f -l -t "$LATE_FILESYSTEMS"
 		    sleep 1s
-		    exit 0
+		    ret=$?
+		    if [ "$ret" -ne "0" ]; then
+		    	umount -a -f -l -t "$LATE_FILESYSTEMS"
+		    	sleep 1s
+		    	exit 0
+		    else
+		    	exit 0
+		    fi
 		fi
 		;;
 esac
